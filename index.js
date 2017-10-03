714b7335dc232bbe64b150ab3fba6d238fd68cc6
mahmne <mahmod_mne@hotmail.com>
2017-10-03 10:22:47 +0300

    create hello word in Express && add middleware log every request METHOD and URL

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+var log = function (req, res, next) {
+  console.log('Logged', req.method, req.url);
+  next();
+};
+
+// create hello word in Express
+app.get('/', function (req, res, next) {
+  res.send('Hello World');
+  next();
+});
+
+// add middleware log every request METHOD and URL
+app.use(log);
+
+// app.use('public', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('app listening in 3000');
+});
